2f7b1ea70cca632f4e4273dc9bc2db48e0c4c904
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-27 11:12:47 +0300

    Reexport ui utils from index

---
+++ src/utils/ui/index.ts
@@ -0,0 +1,7 @@
+export * from "./isInitial";
+export * from "./lazy";
+export * from "./parseCategory";
+export * from "./sendMessage";
+export * from "./sortByCategoryAndCount";
+export * from "./sortByGroupName";
+export * from "./withPreventDefault";
